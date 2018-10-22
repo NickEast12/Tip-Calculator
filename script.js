422ac5db77d6286939f10c71ce9114152fa2efd3
NickEast12 <nickeast1998@gmail.com>
2018-10-22 14:33:56 +0100

    mobile styles + field verification

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ function myFunction() {
 var input = document.getElementById('bill').value;
 var tipRate = document.getElementById('tip').value;
 var people = document.getElementById('people').value;
+// form validation
+if (isNaN(input)) {
+	document.getElementById('yourBill').innerHTML = 
+		"This is not a number!";
+	document.getElementById('yourBill').style.color = "red";
+}
+if (isNaN(tipRate)) {
+	document.getElementById('theTip').innerHTML = 
+		"This is not a number!";
+	document.getElementById('theTip').style.color = "red";
+}
+if (isNaN(people)) {
+	document.getElementById('peopleNo').innerHTML = 
+		"This is not a number!";
+	document.getElementById('peopleNo').style.color = "red";
+}
+
+
+// The POP Up 
 var results = document.getElementById('answers');
 var button = document.getElementById('submit');
 var close = document.getElementById('cross');
@@ -12,11 +31,11 @@ var newTotal =  1 * input + 1 * tipRate;
 var split =   newTotal / people;
 // displaying the answer pop up
 button.onclick = function() {
-	    answers.style.display = "block";
-}
+	answers.style.display = "block";
+};
 close.onclick = function() {
     results.style.display = "none";
-}
+};
 // Pushing answers to HTML
 document.getElementById('answer1').innerHTML = 'Your bill' + ' £' + input;
 document.getElementById('answer2').innerHTML = 'Tip Rate ' + tipRate + '%';
